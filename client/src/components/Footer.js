953334234ab7e963959d16358b674c1f7ca74867
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-05-30 05:20:36 +0300

    location page is ready

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,14 +7,17 @@ export default function Footer() {
 
 
     return (
-    <footer>
-        <div className='container'>
-            <div className='col col-md-2 icons'>
-                <Link className='social-icons'><FontAwesomeIcon icon={faInstagram} /></Link>
-                <Link className='social-icons'><FontAwesomeIcon icon={faTwitter} /></Link>
-                <Link className='social-icons'><FontAwesomeIcon icon={faLinkedin} /></Link>
+        <footer>
+            <div className='container-fluid'>
+                <div className='col col-md-2 icons'>
+                    <Link className='social-icons'><FontAwesomeIcon icon={faInstagram} /></Link>
+                    <Link className='social-icons'><FontAwesomeIcon icon={faTwitter} /></Link>
+                    <Link className='social-icons'><FontAwesomeIcon icon={faLinkedin} /></Link>
+                </div>
+                <div style={{color:'white'}}>
+                &copy; Copyright 2023 - Caffè Exclusivo - All Rights Reserved.
+                </div>
             </div>
-        </div>
-    </footer>)
+        </footer>)
 
 }
--- client/src/pages/LocationAndHours.js
+++ client/src/pages/LocationAndHours.js
@@ -1,8 +1,24 @@
 
-export default function LocationAndHours(){
+export default function LocationAndHours() {
 
-    return(<div>
-        <h2>LocationAndHours!</h2>
-    </div>)
+    return (
+        <div className="wrapper">
+            <h1 className="title">Location</h1>
+            <div className='info d-flex align-items-center'>
+                <div className='col-md-8'>
+                    <img className='cafe_photo' src={require('../assets/cafe_photo.jpg')} alt='cafe' class="img-fluid mb-3 mb-md-0 mx-auto d-block" />
+                </div>
+                <div className='col-md-4'>
+                    <h2>DOWNTOWN</h2>
+                    <p>Street: Pohjolankatu 12, Tampere</p>
+                    <h3>Hours</h3>
+                    <p>Monday to Friday: 7:00 AM - 7:00 PM</p>
+                    <p>Saturday and Sunday: 8:00 AM - 3:00 PM</p>
+                </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d18246.539582739297!2d23.724395147664392!3d61.4941771169008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfi!2sfi!4v1685409925007!5m2!1sfi!2sfi"
+                width="600" height="400"
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>)
 
 }
