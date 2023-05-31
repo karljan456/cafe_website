786f7ae8d72eea6b6c2134ee84a6bf9329cf6613
karljan456 <75903246+karljan456@users.noreply.github.com>
2023-06-01 02:33:02 +0300

    code improvement

--- server/server/src/main/java/com/caffeexclusivo/server/controller/CafeController.java
+++ server/server/src/main/java/com/caffeexclusivo/server/controller/CafeController.java
@@ -1,20 +1,33 @@
 package com.caffeexclusivo.server.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.caffeexclusivo.server.model.Coffee;
+import com.caffeexclusivo.server.model.Cookie;
 import com.caffeexclusivo.server.model.Message;
+import com.caffeexclusivo.server.repository.CoffeeRepository;
+import com.caffeexclusivo.server.repository.CookieRepository;
 import com.caffeexclusivo.server.repository.MessageRepository;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
-public class MessageController {
+public class CafeController {
 	
 	@Autowired
 	MessageRepository messageRepository;
+	
+	@Autowired
+	CookieRepository cookieRepository;
+	
+	@Autowired
+	CoffeeRepository coffeeRepository;
 	
 	@PostMapping("/message")
 	Message addMessage(@RequestBody Message message) {
@@ -22,5 +35,19 @@ public class MessageController {
 		return messageRepository.save(message);
 		
 	}
+	
+	@GetMapping("/getallcookies")
+	List<Cookie> getAllCookies(){
+		return cookieRepository.findAll();
+	}
+	
+	
+	@GetMapping("/getallcoffee")
+	List<Coffee> getAllCoffee(){
+		
+		return coffeeRepository.findAll();
+		
+	}
 
+	
 }
